878ae9528413113f9282d58e6be78ebb71ecb33e
dragon-long-png <long150907@163.com>
2021-01-06 12:04:27 +0800

    模块化细节

--- index.js
+++ index.js
@@ -1,55 +1,30 @@
-// node的全局对象
-// console.log(global);
+// 模块查找，绝对路径
+// require('C:\\Users\\Administrator\\Desktop\\Practice\\Yuan-Node\\node-practice\\a.js')
 
-// setTimeout setInterval 返回的是一个对象，不再是一个数字
-// const timer = setTimeout(() => {
-    
-// }, 400);
+// 相对路径，./  ../
+// require('./a.js');
 
-// console.log(timer);
+// 相对路径，检查内置模块，node_modules
+// require('abc')
 
-// setImmediate 相当于 setTimeout 0
+// 自动补全后缀，  .js .json .node .mjs
+// const result = require('./a');
+// const result = require('./a.json')
+// console.log(result);
+
+// console.log('执行');
+
+// main字段，入口文件
+// require('abc');
 
 
-// __dirname  获取当前模块所在的目录，  并非global属性
+// require('./src')
 
-// console.log(__dirname);
+// reslove函数，返回的是绝对路径，不加载模块
+// console.log(require.resolve('./a.js'));
 
-// console.log(global.__dirname);
 
-// __filename  获取当前模块的文件路径，  并非global属性
+// require('./src')
 
-// console.log(__filename);
-
-// Buffer  自es6, 回头看
-
-// const buffer = Buffer.from('abcdefg', 'utf-8');
-
-// console.log(buffer);
-
-// process
-
-    // cwd, 返回当前进程的工作目录
-    // console.log(process.cwd());
-
-    // exit, 强制退出当前Node进程
-
-    // setTimeout(() => {
-    //     console.log('111');
-    // }, 500);
-    // process.exit()
-
-    // argv, 获取命令中的所有参数 String
-
-    // console.log(process.argv);
-
-    // platform, 获取当前的操作系统 最低及以上版本，并不代表自己电脑的操作系统版本
-    
-    // console.log(process.platform);
-
-    // kill(id), 根据进程id杀死进程
-
-    // process.kill(12760);
-
-    // env, 获取环境变量对象
-    // console.log(process.env);
+const result = require('./myModules')
+console.log(result);
